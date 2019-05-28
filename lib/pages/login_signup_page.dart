49699f2d9029f284189b9903f0ec4c1286386b08
Rexford <seanmavley@gmail.com>
2019-05-28 21:45:55 +0000

    replaced logo asset reference with FlutterLogo()

--- lib/pages/login_signup_page.dart
+++ lib/pages/login_signup_page.dart
@@ -181,14 +181,7 @@ class _LoginSignUpPageState extends State<LoginSignUpPage> {
   Widget _showLogo() {
     return new Hero(
       tag: 'hero',
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
-        child: CircleAvatar(
-          backgroundColor: Colors.transparent,
-          radius: 48.0,
-          child: Image.asset('assets/flutter-icon.png'),
-        ),
-      ),
+      child: FlutterLogo(size: 100.0),
     );
   }
 
