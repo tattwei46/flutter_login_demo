15c937189be72e1f61f69f6f931a61915e201f18
serkanince <serkanince444@gmail.com>
2019-08-20 16:04:59 +0300

    Firebase service bug fix

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -19,15 +19,14 @@ class Auth implements BaseAuth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future<String> signIn(String email, String password) async {
-    // FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(
-    //     email: email, password: password);
-    // return user.uid;
+    AuthResult user = await _firebaseAuth.signInWithEmailAndPassword(email: email,password: password);
+    return user.user.uid;
   }
 
   Future<String> signUp(String email, String password) async {
-    // FirebaseUser user = await _firebaseAuth.createUserWithEmailAndPassword(
-    //     email: email, password: password);
-    // return user.uid;
+    AuthResult user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return user.user.uid;
   }
 
   Future<FirebaseUser> getCurrentUser() async {
